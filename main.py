289b580e80cf69e7447bf9eb642d3273e8d7f0bd
CodingForEntrepreneurs <hello@teamcfe.com>
2024-08-14 15:32:43 -0600

    15 - Using Redis for Rate Limiting

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 from fastapi import FastAPI
 from langserve import add_routes # fastapi
 import helpers
+from decouple import config
+from redis import Redis
 
 app = FastAPI()
+redis_instance = Redis(
+  host=config("REDIS_HOST", cast=str),
+  port=config("REDIS_PORT", cast=int, default=6379),
+  password=config("REDIS_PASSWORD", cast=str),
+  ssl=True
+)
 
 @app.get('/')
 def home_page():
-    return {"hello": "world"}
+    visits_key = "visits"
+    max_requests = 5
+    current_val = redis_instance.get(visits_key)
+    if current_val is None:
+        redis_instance.set(visits_key, 0, 10)
+    redis_instance.incr(visits_key)
+    final_val = redis_instance.get(visits_key)
+    do_rate_limiting = False 
+    try:
+        do_rate_limiting = int(final_val) > max_requests
+    except:
+        pass
+    return {"hello": "world", "visits": final_val, "limited": do_rate_limiting}
 
 chain = helpers.get_chain()
 
