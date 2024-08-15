b3c8464da961bac5d57a946d7d7ea6a14faa4ea5
CodingForEntrepreneurs <hello@teamcfe.com>
2024-08-15 12:21:13 -0600

    17 - API Key Based Rate Limiting

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import helpers
 from decouple import config
 from redis import Redis
 
+API_KEY_HEADER = "X-API-KEY"
+API_ACCESS_USER = config("API_ACCESS_USER")
+API_ACCESS_KEY = config("API_ACCESS_KEY")
+
 app = FastAPI()
 redis_instance = Redis(
   host=config("REDIS_HOST", cast=str),
@@ -16,13 +20,18 @@ redis_instance = Redis(
 @app.middleware("http")
 async def rate_limit_middleware(request: Request, call_next):
     rate_limit = 5
-    rate_window = 10 # seconds
-    visits_key = "visits"
-    current_val = redis_instance.get(visits_key)
-    if current_val is None:
-        redis_instance.set(visits_key, 0, rate_window)
-    redis_instance.incr(visits_key)
-    final_val = redis_instance.get(visits_key)
+    rate_window = 30 # seconds
+    api_key = request.headers.get(API_KEY_HEADER)
+    if api_key is None:
+        return JSONResponse(status_code=400, content={"detail": "Invalid API Key"})
+    if api_key != API_ACCESS_KEY:
+        return JSONResponse(status_code=401, content={"detail": "Invalid API Key"})
+    rate_limit_key = f"rate_limit:{API_ACCESS_USER}"
+    rl_current_val = redis_instance.get(rate_limit_key)
+    if rl_current_val is None:
+        redis_instance.set(rate_limit_key, 0, rate_window)
+    redis_instance.incr(rate_limit_key)
+    final_val = redis_instance.get(rate_limit_key)
     do_rate_limiting = False 
     try:
         do_rate_limiting = int(final_val) > rate_limit
@@ -30,6 +39,11 @@ async def rate_limit_middleware(request: Request, call_next):
         pass
     if do_rate_limiting:
         return JSONResponse(content={"error": "Rate Limited"},status_code=429)
+    access_total_key = f"api_access:{API_ACCESS_USER}"
+    access_current_val = redis_instance.get(access_total_key)
+    if access_current_val is None:
+        redis_instance.set(access_total_key, 0)
+    redis_instance.incr(access_total_key)
     response = await call_next(request)
     return response
 
